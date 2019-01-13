fa0d1d4c2dce0096ca4661f3551da2a567a56e88
Indira Priyadarshini Bobburi <31742324+IndiraBobburi@users.noreply.github.com>
2019-01-12 17:06:36 -0800

    React tutorials to load and delete form data dynamically.

---
+++ src/Image.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Image extends Component {
+	render() {
+        return (
+            <img src = "https://thenypost.files.wordpress.com/2018/10/102318-dogs-color-determine-disesases-life.jpg?quality=90&strip=all&w=618&h=410&crop=1">
+            </img>
+        );
+    }
+}
+
+export default Image;
